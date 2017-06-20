1b57c988ff3ceae8fde82a1c6b937d4975ca3f73
Don <don@Dons-Mac-Pro.local>
2017-06-19 19:45:22 -0700

    clicking works

--- js/app.js
+++ js/app.js
@@ -6,11 +6,12 @@ var counter = 0;
 var limit = 25;
 var temp;
 var i;
-// var display = document.getElementById('display');
+var display = document.getElementById('display');
+var heading = document.getElementById('heading');
 var filenames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
 var Product = function(filename) {
-  this.url = 'images/' + filename;
+  this.url = 'img/' + filename;
   this.clicks = 0;
   this.shown = 0;
   products.push(this);
@@ -20,73 +21,54 @@ for (var i = 0; i < filenames.length; i++) {
   temp = new Product(filenames[i]);
 }
 
-function render(key, i) {
+function render(key) {
   console.log(products[key].url, i);
+  var img = document.createElement('img');
+  products[key].shown += 1;
+  img.setAttribute('src', products[key].url);
+  img.setAttribute('id', key);
+  img.setAttribute('width', "30%");
+  display.append(img);
   // render new graphic based on location in products array and position on page
 }
 
 function choices() {
-  restricted = restricted.slice();
+  display.innerHTML = '';
+  restricted = restricted.slice(3);
   var key = restricted[0];
   for (i = 0; i < 3; i++) {
     while (restricted.indexOf(key) > -1) {
       key = Math.floor(Math.random() * filenames.length);
     }
     restricted.push(key);
-    render(key, i);
+    render(key);
   }
 }
 
 choices();
 
-//
-// function render(product, id) {
-//   console.log(products[product].url);
-//   // products[product].shown += 1;
-//   // var img = document.createElement('img');
-//   // img.setAttribute('src', products[product].url);
-//   // img.setAttribute('id', id);
-//   // display.append(img);
-//   // add html to render image
-// };
-//
-// function presentChoices() {
-//   previous_ids = current_ids.slice(0);
-//   current_ids = [];
-//   while (current_ids.length < 3) {
-//     while(!current_ids.length || current_ids.indexOf(temp) > -1 || previous_ids.indexOf(temp) > -1) {
-//       temp = Math.floor(Math.random() * products.length);
-//     }
-//     render(temp, current_ids.length);
-//     current_ids.push(temp);
-//     products[temp].clicks += 1;
-//   }};
-//
-// function showResults() {
-//   //show results
-// }
-//
-// presentChoices();
-//
-// // display.addEventListener('click', function (event) {
-// //   var id = event.target.getAttribute('id');
-// //   products[id].clicks += 1;
-// //   clicks += 1;
-// //   if (clicks < limit) {
-// //     presentChoices();
-// //   } else {
-// //     showResults();
-// //   };
-// // });
-//
-// // construct objects from filenames
-//
-// // generate three random ids from products
-// // for each id, render image with a unique ID and add 1 to product.shown
-// // if result is listed in previous_ids or current_ids, pick something else
-// // when item is clicked, grab id.  Add 1 to product.clicks
-// // Add 1 to global clicks
-// // previous_ids = current ids
-// // current_ids = []
-// // repeat until clicks = 25
-// // cycle through product list, print results
+function generateRGB() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  if (r == g || g == b || r == b) {
+    return(generateRGB());
+  } else {
+    return('rgb(' + r + ', ' + g + ', ' + b + ')');
+  }
+}
+
+function results() {
+
+}
+
+display.addEventListener('click', function (event) {
+  var answer = event.target.getAttribute('id');
+  console.log(products[answer]);
+  products[answer].clicks += 1;
+  counter += 1;
+  // heading.style.color = generateRGB();
+  choices();
+  document.getElementById('heading').style.color = generateRGB();
+  if counter
+});
